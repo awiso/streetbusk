a128ac4a137aaac09ff8531bba17b256028c56d9
Alexander Wisowaty <alexander@wisowaty.com>
2018-08-21 12:16:59 +0200

    add lat and lng to performances table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_080136) do
+ActiveRecord::Schema.define(version: 2018_08_21_101557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2018_08_21_080136) do
     t.integer "likes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["genre_id"], name: "index_performances_on_genre_id"
     t.index ["user_id"], name: "index_performances_on_user_id"
   end
