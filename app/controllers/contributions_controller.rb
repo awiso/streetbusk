68b9bd00f607ef4e1c5c0df07c72d6bdab0d8cac
Steven Moore <steven.moore177@gmail.com>
2018-08-22 15:06:23 +0200

    implement contributions logic

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,2 +1,24 @@
 class ContributionsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @performance = Performance.find(params[:performance_id])
+    @contribution = Contribution.new(amount: @amount, user: @user, artist: @artist)
+    @contribution.amount = params[:amount][:post]
+    @contribution.artist = @performance.user
+    @contribution.user = current_user
+    authorize @contribution
+    if @contribution.save
+      redirect_to performances_path
+    else
+      render 'performances/show'
+    end
+  end
+
+  private
+
+  def contributions_params
+    params.require(:contribution).permit(:amount, :user, :artist)
+  end
+
 end
