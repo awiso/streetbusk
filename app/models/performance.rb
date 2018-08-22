d5d1462a993bc57aa97bc45f628f274fe488390c
Alexander Wisowaty <alexander@wisowaty.com>
2018-08-22 16:46:25 +0200

    add default photo

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -7,7 +7,6 @@ class Performance < ApplicationRecord
   validates :end_time, presence: true
   validates :description, presence: true
   validates :genre, presence: true
-  validates :photo, presence: true
   # Geocode
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
