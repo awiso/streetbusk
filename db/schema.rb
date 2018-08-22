3e302f3648154a4561d34bc73d0997f1d680d29c
Steven Moore <steven.moore177@gmail.com>
2018-08-22 11:35:22 +0200

    remove contributions from prerformances table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_164038) do
+ActiveRecord::Schema.define(version: 2018_08_22_092316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2018_08_21_164038) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
-    t.integer "contribution_cents"
     t.index ["genre_id"], name: "index_performances_on_genre_id"
     t.index ["user_id"], name: "index_performances_on_user_id"
   end
