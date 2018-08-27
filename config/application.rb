d3a76d0374c0c4247182b58dbe3f1222162bf1ae
Alexander Wisowaty <alexander@wisowaty.com>
2018-08-27 12:27:15 +0200

    sorry guys still dealing with config nonsense

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.action_mailer.delivery_method = :postmark
-config.action_mailer.postmark_settings = { :api_token => "617600f1-7fb1-4550-b4a9-1ea8a6b39e35" }
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,13 +10,15 @@ module Streetbusk
   class Application < Rails::Application
     config.action_view.embed_authenticity_token_in_remote_forms = true
     config.generators do |generate|
-          generate.assets false
-          generate.helper false
-          generate.test_framework  :test_unit, fixture: false
-        end
+      generate.assets false
+      generate.helper false
+      generate.test_framework  :test_unit, fixture: false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    
+    config.action_mailer.delivery_method = :postmark
+    config.action_mailer.postmark_settings = { :api_token => "617600f1-7fb1-4550-b4a9-1ea8a6b39e35" }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
