e0905491c89f59a74184f9960fd70415a35fb737
Alexander Wisowaty <alexanderwisowaty@Alexanders-MBP.localdomain>
2018-08-28 15:38:28 +0200

    add artist profile with crud

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @performances = @user.performances
     authorize @user
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   root to: 'pages#home'
   resources :performances do
     resources :attendance, only: [:create]
     resources :contributions, only: :create
   end
 
-  # get 'dashboard', to: 'artists#show', as: 'artist_dashboard'
   resources :profiles, only: [ :show, :edit, :update ]
   # For creating a new contribution
   resources :contributions, only: [ :show ] do
