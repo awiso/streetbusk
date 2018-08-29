f1ffe8252ab75b3c2e773caa07b5a6e10895d77c
Chris Walsh <chris.w894j@gmail.com>
2018-08-29 19:27:54 +0200

    small changes plus meta tags

--- app/javascript/packs/screenfull_init.js
+++ app/javascript/packs/screenfull_init.js
@@ -1,18 +1,26 @@
 import * as screenfull from 'screenfull';
 
-console.log("aofihoih")
-console.log(screenfull);
-
 function screenfullInit(){
-  const body = document.querySelector('body');
-  body.addEventListener('click', function(){
-    screenfull.request(body);
-    body.classList.add('fullscreen-body');
-
-  })
-    console.log("screenfull enabled", screenfull.enabled);
-
-
+  // const body = document.querySelector('body');
+  // const footerExpand = document.querySelector('#footer-expand');
+  // console.log(footerExpand)
+  //   footerExpand.addEventListener('click', function(e){
+  //     e.preventDefault();
+  //     screenfull.request(body);
+  //    body.classList.add('fullscreen-body');
+  //   })
+  //   footerExpand.addEventListener('touchstart', function(e){
+  //     e.preventDefault();
+  //     screenfull.request(body);
+  //    body.classList.add('fullscreen-body');
+  //   })
+    // body.addEventListener('click', function(e){
+    //   e.preventDefault();
+    //   console.log(e)
+    //   screenfull.request(body);
+    //   body.classList.add('fullscreen-body');
+    //
+    // })
 }
 
 export { screenfullInit };
